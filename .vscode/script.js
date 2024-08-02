6d8f82632770effbd4e7ad52f4d4f95e46904847
emily silva <emily.silv.camp@gmai.com>
2024-08-02 17:43:48 -0300

    cofigurando terminal

--- .vscode/script.js
+++ .vscode/script.js
@@ -3,3 +3,19 @@ const html = document.documentElement
 
 html.classList.toggle('light');
 }
+/*
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+// Servir arquivos estáticos da pasta 'public'
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});*/
